7208d78a9d67466e3d0a6262e4a9a8a64faa7ef8
fernando <fernando.a.gomezsolis@gmail.com>
2025-11-24 02:36:34 -0300

    deploy user

--- FullstackII-FrontP2/src/config/api.ts
+++ FullstackII-FrontP2/src/config/api.ts
@@ -1,2 +1,3 @@
-export const API_BASE_URL = "http://localhost:8081";
-export const API_USERS_URL = "http://52.205.212.116:8082";
+// src/config/api.ts
+export const API_BASE_URL = "http://13.219.43.161:8080";  // backend de productos
+export const API_USERS_URL = "http://52.205.212.116:8082"; // backend de usuarios
